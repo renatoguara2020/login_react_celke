3ef920bd6e56d57ed6500c5c2567aaf6ebd25337
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-07 20:17:27 -0300

    ok

--- login_react_celke/src/App.js
+++ login_react_celke/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './pages/LoginUsuario/index';
+import './App.css';
 
 
 function App() {
