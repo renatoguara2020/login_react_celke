0f67d5080389af7bdfdcad47ef3dfb4693bb2748
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-08 09:50:08 -0300

    commit changes

--- login_react_celke/src/App.js
+++ login_react_celke/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './pages/LoginUsuario/index';
-import formLogin from './pages/LoginUsuario/formLogin';
-import formLogin2 from './pages/LoginUsuario/formLogin2';
 import './App.css';
 
 
@@ -12,9 +10,6 @@ function App() {
       <Router>
         <Switch>
           <Route  path="/login" component={Login}/>
-          <Route  path="/formLogin" component={formLogin}/>
-          <Route  path="/formLogin2" component={formLogin2}/>
-
         </Switch>
       </Router>
     
